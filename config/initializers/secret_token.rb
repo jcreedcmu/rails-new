3f0257578fcdf4594072302279785d6cd81c1f50
Jason Reed <jreed@chef.io>
2015-05-20 09:16:04 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JcreedApp::Application.config.secret_token = '027cf2fa36c0abcef0a2a1ed4e9cb07e08628faecdd2d1a26b6d58a1d8dc49eeb4f81b7cfa6a76e13c2abb7a62fad1b5b1e2a9590b539b87d69a5d059e42717f'
